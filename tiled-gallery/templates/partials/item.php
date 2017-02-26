965097e26f483c583f2a1028fc706ab4969e8264
Wesley Luyten <hi@wesleyluyten.com>
2017-02-26 15:12:45 -0500

    Remove border from link

--- tiled-gallery/templates/partials/item.php
+++ tiled-gallery/templates/partials/item.php
@@ -8,7 +8,7 @@ if ( empty( $item->image_alt ) && ! empty( $item->image_title ) ) {
 ?>
 <div class="tiled-gallery-item<?php if ( isset( $item->size ) ) echo " tiled-gallery-item-$item->size"; ?>" itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
 	<?php if ( $add_link ): ?>
-	<a href="<?php echo $item->link; ?>" data-srcset="<?php echo wp_get_attachment_image_srcset( $item->image->ID, 'large' ); ?>" border="0" itemprop="url" data-fancybox="gallery">
+	<a href="<?php echo $item->link; ?>" data-srcset="<?php echo wp_get_attachment_image_srcset( $item->image->ID, 'large' ); ?>" itemprop="url" data-fancybox="gallery">
 	<?php endif; ?>
 		<meta itemprop="width" content="<?php echo esc_attr( $item->image->width ); ?>">
 		<meta itemprop="height" content="<?php echo esc_attr( $item->image->height ); ?>">
